52ab1cb315d3ca1b9412ef608928f0a90278180b
Arjona <lunaloboez@gmail.com>
2023-07-24 01:23:20 -0400

    a

--- src/main/java/me/arjona/customutilities/Logger.java
+++ src/main/java/me/arjona/customutilities/Logger.java
@@ -7,19 +7,19 @@ import org.bukkit.Bukkit;
 public class Logger {
 
     public void deb(String msg) {
-        msg("&7[&aDEBUG&7] " + msg);
+        msg("&7[&2DEBUG&7] &a" + msg);
     }
 
     public void log(String msg) {
-        msg("&7[&9LOG&7] " + msg);
+        msg("&7[&9LOG&7] &3" + msg);
     }
 
     public void err(String msg) {
-        msg("&7[&4ERROR&7] " + msg);
+        msg("&7[&4ERROR&7] &c" + msg);
     }
 
     public void warn(String msg) {
-        msg("&7[&cWARN&7] " + msg);
+        msg("&7[&cWARN&7] &c" + msg);
     }
 
     private void msg(String msg) {
